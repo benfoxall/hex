1800cb78c6a86ee4940d509fefd1938294dc1348
benfoxall <benfoxall@gmail.com>
2020-12-11 06:17:45 +0000

    Deploying to gh-pages from  @ 3cc961bd990056a5b287297027a1a97131b0ff7d 🚀

---
+++ _dist_/App.js
@@ -0,0 +1,15 @@
+import React from "../web_modules/react.js";
+export const App = () => /* @__PURE__ */ React.createElement(Choose, null);
+const Choose = () => /* @__PURE__ */ React.createElement("main", null, /* @__PURE__ */ React.createElement("p", null, "View content of a ", /* @__PURE__ */ React.createElement("a", {
+  href: "#file"
+}, "file"), ", ", /* @__PURE__ */ React.createElement("a", {
+  href: "#url"
+}, "url"), " or", " ", /* @__PURE__ */ React.createElement("a", {
+  href: "#buffer"
+}, "ArrayBuffer")), /* @__PURE__ */ React.createElement("section", {
+  id: "file"
+}, /* @__PURE__ */ React.createElement("h2", null, "file")), /* @__PURE__ */ React.createElement("section", {
+  id: "url"
+}, /* @__PURE__ */ React.createElement("h2", null, "url")), /* @__PURE__ */ React.createElement("section", {
+  id: "buffer"
+}, /* @__PURE__ */ React.createElement("h2", null, "buffer")));
