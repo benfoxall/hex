ba9aef24d4804a0a5009463047b5e0ebedf2fcf6
Ben Foxall <benfoxall@gmail.com>
2020-12-19 17:41:10 +0000

    Width breakpoints

--- src/Hex.jsx
+++ src/Hex.jsx
@@ -1,26 +1,5 @@
-import React, { useEffect, useState, memo } from "react";
+import React, { useEffect, useState, memo, useMemo } from "react";
 import { useVirtual } from "react-virtual";
-
-const MemoRow = memo(({ index, buffer, bytesPerRow }) => {
-  const byteOffset = index * bytesPerRow;
-
-  const l = Math.min(bytesPerRow, buffer.byteLength - index * bytesPerRow);
-  const u8 = new Uint8Array(buffer, byteOffset, l);
-
-  return (
-    <div className="row">
-      <div>{(index * 4).toString(16).padStart(4, ".")}</div>
-
-      <div>
-        <HexPairs u8={u8} size={bytesPerRow} />
-      </div>
-
-      <div>
-        <Ascii u8={u8} size={bytesPerRow} />
-      </div>
-    </div>
-  );
-});
 
 export const Hex = ({ blob }) => {
   const [buffer, setBuffer] = useState(new ArrayBuffer());
@@ -29,16 +8,12 @@ export const Hex = ({ blob }) => {
     blob.arrayBuffer().then(setBuffer);
   }, [blob]);
 
-  // How many bytes should be displayed in a single row
-  // todo: update by screen width
-  const bytesPerRow = 16;
+  const sm = useMedia("(max-width: 600px)");
+  const md = useMedia("(max-width: 800px)");
+  const lg = useMedia("(max-width: 1400px)");
 
-  /*
-  approach 
-    - on resize (once)
-    - check that we're within bounds
-    - resize if necessary
-  */
+  // How many bytes should be displayed in a single row
+  const bytesPerRow = sm ? 8 : md ? 12 : lg ? 16 : 32;
 
   const view = new DataView(buffer);
   const rows = Math.ceil(view.byteLength / bytesPerRow);
@@ -77,7 +52,7 @@ export const Hex = ({ blob }) => {
                 transform: `translateY(${virtualRow.start}px)`,
               }}
             >
-              <MemoRow
+              <Row
                 index={virtualRow.index}
                 buffer={buffer}
                 bytesPerRow={bytesPerRow}
@@ -90,6 +65,42 @@ export const Hex = ({ blob }) => {
   );
 };
 
+const Row = memo(({ index, buffer, bytesPerRow }) => {
+  const byteOffset = index * bytesPerRow;
+
+  const l = Math.min(bytesPerRow, buffer.byteLength - index * bytesPerRow);
+  const u8 = new Uint8Array(buffer, byteOffset, l);
+
+  return (
+    <div className="row">
+      <div>{(index * bytesPerRow).toString(16).padStart(4, ".")}</div>
+
+      <div>
+        <HexPairs u8={u8} size={bytesPerRow} />
+      </div>
+
+      <div>
+        <Ascii u8={u8} size={bytesPerRow} />
+      </div>
+    </div>
+  );
+});
+
+const useMedia = (query) => {
+  const media = useMemo(() => window.matchMedia(query), [query]);
+  const [active, setActive] = useState(media.matches);
+
+  useEffect(() => {
+    const change = () => setActive(media.matches);
+
+    media.addEventListener("change", change);
+
+    return () => media.removeEventListener("change", change);
+  }, [media]);
+
+  return active;
+};
+
 const HexPairs = ({ u8, size }) => {
   const str = u8.reduce((acc, value) => {
     return acc + value.toString(16).padStart(2, "0") + " ";
@@ -98,8 +109,12 @@ const HexPairs = ({ u8, size }) => {
   return str.padEnd(size * 3 - 1, "_");
 };
 
-const Ascii = ({ u8 }) => {
-  return u8.reduce((acc, value) => {
+const whitespace = /\W/g;
+
+const Ascii = ({ u8, size }) => {
+  const str = u8.reduce((acc, value) => {
     return acc + String.fromCharCode(value);
   }, "");
+
+  return str.replace(whitespace, "·");
 };
