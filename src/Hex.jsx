25eb608a89b68f3fe9f7b4ba659285a8af603966
Ben Foxall <benfoxall@gmail.com>
2020-12-19 16:16:21 +0000

    Switch over to react-virtual

--- src/Hex.jsx
+++ src/Hex.jsx
@@ -1,4 +1,26 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, memo } from "react";
+import { useVirtual } from "react-virtual";
+
+const MemoRow = memo(({ index, buffer, bytesPerRow }) => {
+  const byteOffset = index * bytesPerRow;
+
+  const l = Math.min(bytesPerRow, buffer.byteLength - index * bytesPerRow);
+  const u8 = new Uint8Array(buffer, byteOffset, l);
+
+  return (
+    <div className="row">
+      <div>{(index * 4).toString(16).padStart(4, ".")}</div>
+
+      <div>
+        <HexPairs u8={u8} />
+      </div>
+
+      <div>
+        <Ascii u8={u8} />
+      </div>
+    </div>
+  );
+});
 
 export const Hex = ({ blob }) => {
   const [buffer, setBuffer] = useState(new ArrayBuffer());
@@ -11,9 +33,6 @@ export const Hex = ({ blob }) => {
   // todo: update by screen width
   const bytesPerRow = 16;
 
-  const view = new DataView(buffer);
-  const rows = Math.ceil(view.byteLength / bytesPerRow);
-
   /*
   approach 
     - on resize (once)
@@ -21,52 +40,70 @@ export const Hex = ({ blob }) => {
     - resize if necessary
   */
 
+  const view = new DataView(buffer);
+  const rows = Math.ceil(view.byteLength / bytesPerRow);
+
+  const parentRef = React.useRef();
+
+  const rowVirtualizer = useVirtual({
+    size: rows,
+    parentRef,
+    estimateSize: React.useCallback(() => 20, []),
+    overscan: 5,
+  });
+
   return (
-    <table>
-      <tbody>
-        {repeat(rows, (i) => {
-          const l = Math.min(bytesPerRow, buffer.byteLength - i * bytesPerRow);
-          const sub = new DataView(buffer, i * bytesPerRow, l);
-
-          return (
-            <tr key={i}>
-              <td>{(i * 4).toString(16).padStart(4, ".")}</td>
-
-              <td>
-                <HexPairs view={sub} />
-              </td>
-
-              <td>
-                <Ascii view={sub} />
-              </td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+    <>
+      <div
+        ref={parentRef}
+        className="List"
+        style={{
+          height: `200px`,
+          width: `800px`,
+          overflow: "auto",
+        }}
+      >
+        <div
+          style={{
+            height: `${rowVirtualizer.totalSize}px`,
+            width: "100%",
+            position: "relative",
+          }}
+        >
+          {rowVirtualizer.virtualItems.map((virtualRow) => (
+            <div
+              key={virtualRow.index}
+              className={virtualRow.index % 2 ? "ListItemOdd" : "ListItemEven"}
+              style={{
+                position: "absolute",
+                top: 0,
+                left: 0,
+                width: "100%",
+                height: `${virtualRow.size}px`,
+                transform: `translateY(${virtualRow.start}px)`,
+              }}
+            >
+              <MemoRow
+                index={virtualRow.index}
+                buffer={buffer}
+                bytesPerRow={bytesPerRow}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   );
 };
 
-const HexPairs = ({ view }) => {
-  let acc = "";
-
-  for (let i = 0; i < view.byteLength; i++) {
-    acc += view.getUint8(i).toString(16).padStart(2, ".") + " ";
-  }
-
-  return acc;
+const HexPairs = ({ u8 }) => {
+  return u8.reduce((acc, value) => {
+    return acc + value.toString(16).padStart(2, "0") + " ";
+  }, "");
 };
 
-const Ascii = ({ view }) => {
-  let acc = "";
-
-  for (let i = 0; i < view.byteLength; i++) {
-    acc += String.fromCharCode(view.getUint8(i));
-  }
-
-  return acc;
+const Ascii = ({ u8 }) => {
+  return u8.reduce((acc, value) => {
+    return acc + String.fromCharCode(value);
+  }, "");
 };
-
-function repeat(length, fn) {
-  return Array.from({ length }, (_, i) => fn(i));
-}
