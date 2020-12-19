a6b255c8f0da8bbf61985142dd6d54b72a0a5391
benfoxall <benfoxall@gmail.com>
2020-12-19 17:42:08 +0000

    Deploying to gh-pages from  @ 836ad6c2bd2e68e1ec65ceb566a3c06cfe8bbe21 🚀

--- _dist_/Hex.js
+++ _dist_/Hex.js
@@ -1,23 +1,14 @@
-import React, {useEffect, useState, memo} from "../web_modules/react.js";
+import React, {useEffect, useState, memo, useMemo} from "../web_modules/react.js";
 import {useVirtual} from "../web_modules/react-virtual.js";
-const MemoRow = memo(({index, buffer, bytesPerRow}) => {
-  const byteOffset = index * bytesPerRow;
-  const l = Math.min(bytesPerRow, buffer.byteLength - index * bytesPerRow);
-  const u8 = new Uint8Array(buffer, byteOffset, l);
-  return /* @__PURE__ */ React.createElement("div", {
-    className: "row"
-  }, /* @__PURE__ */ React.createElement("div", null, (index * 4).toString(16).padStart(4, ".")), /* @__PURE__ */ React.createElement("div", null, /* @__PURE__ */ React.createElement(HexPairs, {
-    u8
-  })), /* @__PURE__ */ React.createElement("div", null, /* @__PURE__ */ React.createElement(Ascii, {
-    u8
-  })));
-});
 export const Hex = ({blob}) => {
   const [buffer, setBuffer] = useState(new ArrayBuffer());
   useEffect(() => {
     blob.arrayBuffer().then(setBuffer);
   }, [blob]);
-  const bytesPerRow = 16;
+  const sm = useMedia("(max-width: 600px)");
+  const md = useMedia("(max-width: 800px)");
+  const lg = useMedia("(max-width: 1400px)");
+  const bytesPerRow = sm ? 8 : md ? 12 : lg ? 16 : 32;
   const view = new DataView(buffer);
   const rows = Math.ceil(view.byteLength / bytesPerRow);
   const parentRef = React.useRef();
@@ -29,42 +20,62 @@ export const Hex = ({blob}) => {
   });
   return /* @__PURE__ */ React.createElement(React.Fragment, null, /* @__PURE__ */ React.createElement("div", {
     ref: parentRef,
-    className: "List",
     style: {
-      height: `200px`,
-      width: `800px`,
       overflow: "auto"
     }
   }, /* @__PURE__ */ React.createElement("div", {
     style: {
       height: `${rowVirtualizer.totalSize}px`,
-      width: "100%",
       position: "relative"
     }
   }, rowVirtualizer.virtualItems.map((virtualRow) => /* @__PURE__ */ React.createElement("div", {
     key: virtualRow.index,
-    className: virtualRow.index % 2 ? "ListItemOdd" : "ListItemEven",
     style: {
       position: "absolute",
       top: 0,
       left: 0,
-      width: "100%",
-      height: `${virtualRow.size}px`,
       transform: `translateY(${virtualRow.start}px)`
     }
-  }, /* @__PURE__ */ React.createElement(MemoRow, {
+  }, /* @__PURE__ */ React.createElement(Row, {
     index: virtualRow.index,
     buffer,
     bytesPerRow
   }))))));
 };
-const HexPairs = ({u8}) => {
-  return u8.reduce((acc, value) => {
+const Row = memo(({index, buffer, bytesPerRow}) => {
+  const byteOffset = index * bytesPerRow;
+  const l = Math.min(bytesPerRow, buffer.byteLength - index * bytesPerRow);
+  const u8 = new Uint8Array(buffer, byteOffset, l);
+  return /* @__PURE__ */ React.createElement("div", {
+    className: "row"
+  }, /* @__PURE__ */ React.createElement("div", null, (index * bytesPerRow).toString(16).padStart(4, ".")), /* @__PURE__ */ React.createElement("div", null, /* @__PURE__ */ React.createElement(HexPairs, {
+    u8,
+    size: bytesPerRow
+  })), /* @__PURE__ */ React.createElement("div", null, /* @__PURE__ */ React.createElement(Ascii, {
+    u8,
+    size: bytesPerRow
+  })));
+});
+const useMedia = (query) => {
+  const media = useMemo(() => window.matchMedia(query), [query]);
+  const [active, setActive] = useState(media.matches);
+  useEffect(() => {
+    const change = () => setActive(media.matches);
+    media.addEventListener("change", change);
+    return () => media.removeEventListener("change", change);
+  }, [media]);
+  return active;
+};
+const HexPairs = ({u8, size}) => {
+  const str = u8.reduce((acc, value) => {
     return acc + value.toString(16).padStart(2, "0") + " ";
   }, "");
+  return str.padEnd(size * 3 - 1, "_");
 };
-const Ascii = ({u8}) => {
-  return u8.reduce((acc, value) => {
+const whitespace = /\W/g;
+const Ascii = ({u8, size}) => {
+  const str = u8.reduce((acc, value) => {
     return acc + String.fromCharCode(value);
   }, "");
+  return str.replace(whitespace, "\xB7");
 };
