fb31acc7a3997cf038fa4cbeac68c0ecb17e0bf8
benfoxall <benfoxall@gmail.com>
2020-12-30 22:03:25 +0000

    Deploying to gh-pages from  @ 713e7f84aaf47c95b1ba332f1c77fd727f536f76 🚀

--- _dist_/App.js
+++ _dist_/App.js
@@ -40,19 +40,37 @@ const testfile = new File([
   "and some other stuff here!!DSF",
   234
 ], "test-file.fob");
-export const App = () => /* @__PURE__ */ React.createElement(Choose, null);
 const useObjectURL = (blob) => {
   const url = useMemo(() => blob && URL.createObjectURL(blob), [blob]);
   useEffect(() => () => url && URL.revokeObjectURL(url), [url]);
   return url;
 };
-const Choose = () => {
-  const [file, setFile] = useState(testfile);
+const ex = `
+hex = window.open("http://localhost:8080/", 'hex', 'width=300');
+
+hex.postMessage(new Uint8Array([1,2,3,4]), '*');
+`;
+export const App = () => {
+  const [file, setFile] = useState();
   const objectURL = useObjectURL(file);
   const close = () => setFile(void 0);
   const onDrop = (files) => {
     setFile(files[0]);
   };
+  useEffect(() => {
+    const listen = (e) => {
+      console.log(e);
+      const data = e.data;
+      const ab = data instanceof ArrayBuffer ? data : data.buffer instanceof ArrayBuffer ? data.buffer : null;
+      if (ab) {
+        setFile(new File([ab], `${e.origin}.data`));
+      }
+    };
+    window.addEventListener("message", listen);
+    return () => {
+      window.removeEventListener("message", listen);
+    };
+  }, []);
   const {getRootProps, getInputProps, isDragActive, open} = useDropzone({
     onDrop,
     noClick: true,
@@ -69,12 +87,16 @@ const Choose = () => {
     href: objectURL,
     download: file.name,
     title: "Download content"
-  }, "\u2193"), /* @__PURE__ */ React.createElement("button", {
+  }, "\u2193 Save"), /* @__PURE__ */ React.createElement("button", {
     onClick: close,
     title: "Close file"
-  }, "\u2A2F"))))), file && /* @__PURE__ */ React.createElement(Hex2, {
+  }, "Close"))))), file ? /* @__PURE__ */ React.createElement(Hex2, {
     blob: file
-  }));
+  }) : /* @__PURE__ */ React.createElement("section", {
+    className: "info"
+  }, /* @__PURE__ */ React.createElement("p", null, "A viewer for binary data"), /* @__PURE__ */ React.createElement("ul", null, /* @__PURE__ */ React.createElement("li", null, /* @__PURE__ */ React.createElement("a", {
+    onClick: open
+  }, "Select a local file")), /* @__PURE__ */ React.createElement("li", null, "Post an ArrayBuffer")), /* @__PURE__ */ React.createElement("code", null, /* @__PURE__ */ React.createElement("pre", null, ex))));
 };
 const units = ["byte", "kilobyte", "megabyte", "gigabyte"];
 const Size = memo(({bytes}) => {
