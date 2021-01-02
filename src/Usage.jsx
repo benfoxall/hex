90d5ae9ab739c64a9d8955761b046af919b63acc
Ben Foxall <benfoxall@gmail.com>
2021-01-02 18:27:32 +0000

    Change usage for safari console

--- src/Usage.jsx
+++ src/Usage.jsx
@@ -7,14 +7,13 @@ const helperUrl = help.href;
 
 /*
 console.log(require('highlight.js').highlight('js',
-  'const hex = await import("url")\nhex.view(new Uint8Array([1, 2, 3, 4]))'
+  'hex = await import("url")\nhex.view(new Uint8Array([1, 2, 3, 4]))'
 ).value)
 */
 
 export const Usage = () => (
   <pre className="hljs">
-    <span className="hljs-keyword">const</span> hex ={" "}
-    <span className="hljs-keyword">await</span>{" "}
+    hex = <span className="hljs-keyword">await</span>{" "}
     <span className="hljs-keyword">import</span>(
     <span className="hljs-string">&quot;{helperUrl}&quot;</span>)<br />
     <br />
