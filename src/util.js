68418bb963ac584345cd2a4f1bb85a6a2ff58193
Ben Foxall <benfoxall@gmail.com>
2021-01-02 18:19:03 +0000

    use pretty-bytes for formatting
    Intl.NumberFormat doesn't support bytes in iOS

--- src/util.js
+++ src/util.js
@@ -1,4 +1,5 @@
 import { useEffect, useMemo, useState, memo } from "react";
+import prettyBytes from "pretty-bytes";
 
 export const useObjectURL = (blob) => {
   const url = useMemo(() => blob && URL.createObjectURL(blob), [blob]);
@@ -23,21 +24,4 @@ export const useMedia = (query) => {
   return active;
 };
 
-const units = ["byte", "kilobyte", "megabyte", "gigabyte"];
-export const ByteSize = memo(({ bytes }) => {
-  let s = bytes;
-
-  for (const unit of units) {
-    if (s < 750) {
-      return new Intl.NumberFormat("en", {
-        style: "unit",
-        unitDisplay: "narrow",
-        unit,
-      }).format(Math.round(s));
-    }
-
-    s /= 1000;
-  }
-
-  return "huge";
-});
+export const ByteSize = memo(({ bytes }) => prettyBytes(bytes));
